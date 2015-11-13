206c5fcc5ab47d316d3a371d123fc9e9c0c541b3
RickYeh <RickYeh@users.noreply.github.com>
2015-11-12 17:08:52 -0800

    Added new game generation and storage into memory database

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var rand = require('generate-key');
 
 var listenPort = 12345;
+
+// Dictionary
+var dictionary = ['macbook', 'surface'];
+
+// Object to hold ongoing games
+var gameDatabase = {};
 
 // Dummy Puzzle for now
 var puzzle = {
@@ -15,38 +22,50 @@ var puzzle = {
 
 // BodyParser
 
-app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
-
-app.use(function(req, res) {
-    var obj = req.body;
-    var email = obj.email;
-    var response = {};
-
-    response = puzzle;
-
-    console.log('Body Parser Used');
-    console.log(JSON.stringify(req.body));
-
-    res.setHeader('Content-Type', 'text/plain');
-
-    res.end(JSON.stringify(response));
-    // res.end(JSON.stringify(req.body, null, 2));
-})
-
 
 app.get('/', function(req, res) {
     res.send('get message received');
     console.log('get received');
 });
 
-
+// Post received to root - New Game Generation
 app.post('/', function(req, res) {
-    res.send('POST received');
-    res.send(req.body)
-    console.log(req.body);
-    console.log('Post received');
+    var newGame = {};
+    newGame.game_key = rand.generateKey();
+    newGame.puzzle = dictionary[0]; // Random word here later
+    newGame.num_tries_left = 5;
+    newGame.state = 'alive';
+
+    gameDatabase[newGame.game_key] = newGame;
+
+    console.log();
+    console.log('New Game requested received with email: ' + req.body.email);
+    console.log('Key generated : ' + newGame.game_key);
+
+    var response = {
+        game_key: '',
+        phrase: '',
+        state: '',
+        num_tries_left: 0
+    };
+
+    console.log('Game Database is currently:');
+    printDatabase();
+
+
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.end(JSON.stringify(response));
+
+
+    // res.end(JSON.stringify(req.body, null, 2));
 })
+
+function printDatabase() {
+    for (var k in gameDatabase) {
+        console.log(k + ' - ' + gameDatabase[k].puzzle);
+    }
+}
 
 app.listen(process.env.PORT || listenPort);
 console.log('Server running at localhost:' + listenPort);
